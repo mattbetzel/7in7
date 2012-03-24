fd637e06120423af636d845d6bfe5d960f4d66d6
Matt Betzel <matt.betzel@opower.com>
2012-03-24 15:08:10 +0000

    Added support for error conditions with Maybe and added main function

--- haskell/rpn.hs
+++ haskell/rpn.hs
@@ -1,20 +1,40 @@
 import Data.Char (isSpace, isDigit)
 import Data.List (groupBy)
+import Data.Maybe (listToMaybe)
+import System.Environment (getArgs)
 
 input = "10 4 3 + 2 * -"
-input' = "  10  4  3  +  2 * -"
-input'' = "  10.3  4.2  3.0  +  2 * -"
+inputSpace = "  10  4  3  +  2 * -"
+inputDouble = "  10.3  4.2  3.0  +  2 * -"
+inputBad = "sdf"
+inputUnbalanced = "4 3 2 +"
+inputBadOp = "4 +"
 
-rpn :: (Num a, Read a) => String -> a
-rpn = head . foldl stacker [] . words
-    where stacker s t = case parseToken t of
-            (Just (Left num)) -> num : s
-            (Just (Right op)) -> performOp op s
+main = do
+    (x:_) <- getArgs
+    maybe (putStrLn "Invalid input.") print (rpn x)
+
+rpn :: (Num a, Read a) => String -> Maybe a
+rpn str = computeStack str >>= justHead
+
+justHead :: [a] -> Maybe a
+justHead (x:[]) = Just x
+justHead _      = Nothing
+
+computeStack :: (Num a, Read a) => String -> Maybe [a]
+computeStack = foldl stacker (Just []) . words
+
+stacker :: (Num a, Read a) => Maybe [a] -> String -> Maybe [a]
+stacker m t = m >>= (\s -> case parseToken t of
+        (Just (Left num)) -> Just $ num : s
+        (Just (Right op)) -> performOp op s
+        Nothing           -> Nothing)
 
 type Op a = a -> a -> a
 
-performOp :: Op a -> [a] -> [a]
-performOp op (x:y:xs) = op y x : xs
+performOp :: Op a -> [a] -> Maybe [a]
+performOp op (x:y:xs) = Just $ op y x : xs
+performOp _  _        = Nothing
 
 parseToken :: (Read a, Num a) => String -> Maybe (Either a (Op a))
 parseToken str = case getNum str of
